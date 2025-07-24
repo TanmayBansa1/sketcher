c225ffaaeca62af0003b978d82b12b0ea491fa1d
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-07-24 22:25:51 +0530

    Main qa (#2)
    * chat schemas
    
    * added slug and room schemas
    
    * get chats route
    
    * added exception handling to the http layert
    
    * working, resolved all type issues
    
    * removed unnecessary include param from tsconfig.json
    
    * working http layer

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,38 @@
 import { WebSocketServer, WebSocket } from 'ws';
+import jwt from 'jsonwebtoken';
+import { DecodedTokenSchema } from '@repo/commons/authSchemas';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const wss = new WebSocketServer({ port: 8080 });
 
 const rooms : Record<string, WebSocket[]> = {};
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, request) {
+  const url = request.url;
+  const token = new URLSearchParams(url?.split('?')[1] || '').get('token');
+  if(!token){
+    console.log('No token provided');
+    ws.send(JSON.stringify({
+      type: 'error',
+      message: 'No token provided'
+    }));
+    ws.close();
+    return;
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as DecodedTokenSchema;
+
+  if(!decoded.userId){
+    console.log('Invalid token');
+    ws.send(JSON.stringify({
+      type: 'error',
+      message: 'Invalid token'
+    }));
+    ws.close();
+    return;
+  }
+
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
